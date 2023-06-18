a039c2243fd3e463c8eab647ed013cc5686afae9
Tetiana2002 <125501735+Tetiana2002@users.noreply.github.com>
2023-06-18 17:14:59 +0200

    update

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  body: document.querySelector("body").style,
+  changeColor: document.querySelector(".change-color"),
+  textValue: document.querySelector(".color")
+};
+refs.changeColor.addEventListener("click", changeColor);
+function changeColor() {
+  const color = getRandomHexColor();
+  refs.body.backgroundColor = color;
+  refs.textValue.textContent = color
+};
